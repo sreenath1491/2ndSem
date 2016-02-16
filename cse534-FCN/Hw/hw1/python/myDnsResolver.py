eaadac5a2a685f708cf981857c967157c6400f6a
sreenath1491 <sreenath1491@gmail.com>
2016-02-16 01:33:59 -0500

    need to fix cname

--- cse534-FCN/Hw/hw1/python/myDnsResolver.py
+++ cse534-FCN/Hw/hw1/python/myDnsResolver.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import dns.resolver
 import dns.rdatatype
 import dns.message
@@ -8,8 +9,13 @@ import dns.rcode
 rootIndex = -1
 rootsQueried = 0
 ROUND_ROBIN_FILE = "./rrData.txt"
+rootList = ['a.root-servers.net', 'b.root-servers.net', 'c.root-servers.net', 'd.root-servers.net', 'e.root-servers.net',
+            'f.root-servers.net', 'g.root-servers.net', 'h.root-servers.net', 'i.root-servers.net', 'j.root-servers.net',
+            'k.root-servers.net', 'l.root-servers.net', 'm.root-servers.net']
 
 def run(domain):
+    global rootIndex
+    global rootsQueried
     if os.path.isfile(ROUND_ROBIN_FILE):
         try:
             f = open(ROUND_ROBIN_FILE)
@@ -21,87 +27,72 @@ def run(domain):
         rootIndex = 0
 
     rootsQueried = 0
-
     input = domain
     if input[-1] != '.':
         input += '.'
 
-    dots = len(input) - len(input.replace('.', ''))
-    # Record[] records = queryAllRoots(input, dots);
-    # ArrayList<String> results = getResults(records);
-    #
-    # if(results.size() == 0){
-    #     System.out.println("ERROR: Could not obtain the ip address of input");
-    # }
-    # else{
-    #     for(String s : results){
-    #         System.out.println(s.split("/")[1]);
-    #     }
-    # }
-    #
-    # try{
-    #     updateFile(f);
-    # }
-    # catch(Exception e){
-    #     System.out.println("File root index update failed");
-    #     return;
-    # }
+    response = queryAllRoots(input)
+    if not response:
+        print 'ERROR: Could not obtain the ip address of input'
+    try:
+        f = open(ROUND_ROBIN_FILE, 'w+')
+        f.write(str(rootIndex))
+        f.write('\n')
+        f.write('Last used root: '+rootList[rootIndex])
+        f.flush()
+        f.close()
+    except:
+        print "ERROR occurred while updating round robin file"
 
-# def Record[] queryAllRoots(input, dots):
-#     root;
-#     Record[] records = null;
-#     while((records == null || records.length == 0)&& rootsQueried < rootList.size()):
-#         rootIndex = (rootIndex +1)%13;
-#         root = rootList.get(rootIndex);
-#         records = getFinalAddress(root, input, dots);
-#         rootsQueried++;
-#         //System.out.println("Query number: "+rootsQueried);
-#         //System.out.println("Query index: "+rootIndex);
-#     return records;
 
-def getFinalAddress(address, domain, dots):
-        default = dns.resolver.get_default_resolver()
-        address = default.query(address).rrset[0].to_text()
-    #try:
-        while dots > 0:
-            resolver = dns.resolver.Resolver()
-            resolver.nameservers  =  ['202.12.27.33']
+def queryAllRoots(input):
+    response = False
+    global rootsQueried
+    while not response and rootsQueried < len(rootList):
+        global rootIndex
+        rootIndex = (rootIndex +1)%13
+        root = rootList[rootIndex]
+        response = getFinalAddress(root, input)
+        rootsQueried += 1
+    return response
+
+def getFinalAddress(address, domain):
+    simpleResolver = dns.resolver.get_default_resolver()
+    answerSection = []
+    try:
+        while True:
+            address = simpleResolver.query(address).rrset[0].to_text()
             query = dns.message.make_query(domain, dns.rdatatype.A)
             response = dns.query.udp(query, address)
             rcode = response.rcode()
             if rcode != dns.rcode.NOERROR:
                 if rcode == dns.rcode.NXDOMAIN:
-                    raise Exception('%s does not exist.' % domain)
+                    print ('%s does not exist.' % domain)
+                    sys.exit(0)
                 else:
-                    raise Exception('Error %s' % dns.rcode.to_text(rcode))
-
-            rrset = None
-            if len(response.authority) > 0 and dots > 1:
-                rrset = response.authority[0]
+                    return False
+            authoritySection = response.authority
+            answerSection = response.answer
+            if len(answerSection) > 0 and answerSection[0].rdtype == dns.rdatatype.A:
+                break
+            elif len(answerSection) > 0 and answerSection[0].rdtype == dns.rdatatype.CNAME:
+                domain = answerSection[0][0].target
+            elif len(authoritySection) > 0 and authoritySection[0].rdtype == dns.rdatatype.NS:
+                address = authoritySection[0][0].target
             else:
-                rrset = response.answer[0]
+                break
+        if len(answerSection) > 0:
+            #print answerSection[0].to_text()
+            print [x.address for x in answerSection[0].items]
+            return True
+        else:
+            return False
+    except:
+        return False
 
-            rr = rrset[0]
-            if rr.rdtype == dns.rdatatype.SOA:
-                print 'Same server is authoritative for %s' % domain
-            else:
-                authority = rr.target
-                address = default.query(authority).rrset[0].to_text()
-                print '%s is authoritative for %s' % (authority, domain)
-                #nameserver = default.query(authority).rrset[0].to_text()
-            dots -= 1
-            #depth += 1
-            #if dots > 1:
-             #   print "in if"
-               #records = resp.getSectionArray(Section.AUTHORITY);
-               # address = ((NSRecord) records[2]).getAdditionalName().toString();
-            #dots -= 1
-
-        #records = resp.getSectionArray(Section.ANSWER);
-        #return records;
-    #except:
-        print "ERROR"
-        #return [];
-
-
-getFinalAddress("a.root-servers.net", "www.facebook.com", 3)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print 'Please enter the domain to query'
+    else:
+        #run(sys.argv[1])
+        run('www.facebook.com')
